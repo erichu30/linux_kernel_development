8c58dea9f8f0316630d1ce216bc11d3c624216a5
erichuNotebook <chiawei30@gmail.com>
2022-06-05 00:26:00 +0800

    append hw3

---
+++ hw3/number_of_files_opened.c
@@ -0,0 +1,23 @@
+#include <linux/kernel.h>
+#include <linux/linkage.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+
+asmlinkage long sys_number_of_files_opened(void)
+{
+	long cnt = 0;
+    struct task_struct *task;
+	for_each_process(task) {
+		/* this pointlessly prints the name, PID, and state of each task */
+		/* task's state (-1 unrunnable, 0 runnable, > 0 stopped) */
+		printk("%s[%d]_%ld\n", task->comm, task->pid, task->state);
+		cnt += 1;
+	}
+
+	return cnt;
+}
+
+SYSCALL_DEFINE0(number_of_files_opened) {
+    return sys_number_of_files_opened();
+}
+
