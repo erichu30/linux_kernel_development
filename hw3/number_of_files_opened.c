6f0ac41f3e837b095cf9c4a5921541f73a47c42c
ericServerTest <chiawei30@gmail.com>
2022-06-04 16:47:01 +0000

    update number_of_files_opened.c

--- hw3/number_of_files_opened.c
+++ hw3/number_of_files_opened.c
@@ -3,10 +3,9 @@
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 
-asmlinkage long sys_number_of_files_opened(void)
+asmlinkage long sys_number_of_files_opened(int pid)
 {
-	long cnt = 0;
-    struct task_struct *task;
+	int cnt;	// define the counting of number of files opened by specfic PID
 	for_each_process(task) {
 		/* this pointlessly prints the name, PID, and state of each task */
 		/* task's state (-1 unrunnable, 0 runnable, > 0 stopped) */
@@ -17,7 +16,7 @@ asmlinkage long sys_number_of_files_opened(void)
 	return cnt;
 }
 
-SYSCALL_DEFINE0(number_of_files_opened) {
-    return sys_number_of_files_opened();
+SYSCALL_DEFINE1(number_of_files_opened, int, pid) {
+    return sys_number_of_files_opened(pid);
 }
 
--- hw3/test3.c
+++ hw3/test3.c
@@ -3,12 +3,11 @@
 #include <sys/syscall.h>
 
 #define SYS_number_of_files_opened 436
-#define number_of_files_opened() syscall(SYS_number_of_files_opened)
 
 int main(){
     */Write a system call, number_of_files_opened(int pid),  which a C program can invoke to report the number of files currently opened by a particular process identified by the user from the command line.*/
 	int pid;
 	printf("Please enter the process id\n");
     	scanf("%d", &pid);
-	printf("There are %d files currently opened by the process id %d\n", number_of_files_opened(pid), pid);
+	printf("There are %d files currently opened by the process id %d\n", syscall(SYS_number_of_files_opened, pid), pid);
 }
