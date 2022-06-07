9cd0a70b179b87f4cedccec515174cbdc9f46e43
ericServerTest <chiawei30@gmail.com>
2022-06-07 13:31:04 +0000

    hw3 done

--- hw3/number_of_files_opened.c
+++ hw3/number_of_files_opened.c
@@ -2,21 +2,37 @@
 #include <linux/linkage.h>
 #include <linux/syscalls.h>
 #include <linux/sched.h>
+#include <linux/fdtable.h>
 
-asmlinkage long sys_number_of_files_opened(int pid)
+asmlinkage long sys_number_of_files_opened(int _pid)
 {
-	int cnt;	// define the counting of number of files opened by specfic PID
+	struct task_struct *task;
+	long opened_files_number = 0;
 	for_each_process(task) {
-		/* this pointlessly prints the name, PID, and state of each task */
-		/* task's state (-1 unrunnable, 0 runnable, > 0 stopped) */
-		printk("%s[%d]_%ld\n", task->comm, task->pid, task->state);
-		cnt += 1;
+		if (task->pid != _pid){
+			continue;
+		}
+
+		// Ignore processes without files
+    		if (task->files == NULL){
+        		continue;
+		}
+
+		/* fd_array: array of file objects */
+		printk("%s[%d]\n", task->comm, task->pid);
+
+		struct file** f = task->files->fdt->fd;
+		while(*f){
+			opened_files_number += 1;
+			f++;
+		}
+		printk("Number of files opened: %ld\n", opened_files_number);
 	}
 
-	return cnt;
+	return opened_files_number;
 }
 
-SYSCALL_DEFINE1(number_of_files_opened, int, pid) {
-    return sys_number_of_files_opened(pid);
+SYSCALL_DEFINE1(number_of_files_opened, int, _pid) {
+    return sys_number_of_files_opened(_pid);
 }
 
